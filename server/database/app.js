3e4df0aaba86aa7b175e678b8a0318bb88e7ddc4
Lenity Tumelo Mabilo <lenityt@gmail.com>
2025-08-16 00:34:05 -0400

    Update dealership routes

--- server/database/app.js
+++ server/database/app.js
@@ -58,18 +58,39 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
-});
+    try {
+      const documents = await Dealerships.find();
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
+  });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
-});
+    try {
+      const documents = await Dealerships.find({ state: req.params.state });
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
+  });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
-});
+    try {
+      const dealerId = Number(req.params.id); // convert to number
+      const document = await Dealerships.findOne({ id: dealerId });
+      if (!document) {
+        return res.status(404).json({ error: 'Dealer not found' });
+      }
+      res.json(document);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error fetching document' });
+    }
+  });
+  
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
