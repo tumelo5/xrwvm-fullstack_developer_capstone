6f94ce2c2d335678598f81359141d01a2639acab
Lenity Tumelo Mabilo <lenityt@gmail.com>
2025-08-16 05:32:19 -0400

    Made changes to veiws,url,.env and respapis

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,16 +7,24 @@ from .views import registration
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
     path('register/', registration, name='register'),
+    
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for add a review view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    
+    path(route='add_review', view=views.add_review, name='add_review'),
     # path for logout request
     path('logout/', view=views.logout_request, name='logout'),
+    
+    #path for get_deaker_id
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
